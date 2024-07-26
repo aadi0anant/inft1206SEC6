b1b365f259e47c6f8d0505a6634b682904c0a91a
aadi0anant <ritik.sharmacollege@gmail.com>
2024-07-26 09:04:24 -0400

    For Loop
    Declared constant array listing file names and declared a constant object containing alternative text for images. Started a for loop which will embed all image in the page along with its alternative text.

--- Assignmentshtml/4assignment/part_2.js
+++ Assignmentshtml/4assignment/part_2.js
@@ -5,14 +5,24 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const alternativeText = {
+    'pic1.jpg' : 'Eye',
+    'pic2.jpg' : 'Mountain',
+    'pic3.jpg' : 'Flower',
+    'pic4.jpg' : 'Egyptian',
+    'pic5.jpg' : 'Butterfly'
+  }
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alternativeText[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', displayedImage.src = target.src);
+    newImage.addEventListener('click', displayedImage.alt = target.alt);
+}
 /* Wiring up the Darken/Lighten button */
